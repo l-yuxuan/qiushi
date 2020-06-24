544e423985cdc31740ba96d32a0f5dd7dc9a560d
l-yuxuan <1062614218@qq.com>
2020-06-24 11:42:39 +0800

    我修改了uni-app

---
+++ qiushibaike/pages/dome/dome.vue
@@ -0,0 +1,35 @@
+<template>
+	<view>
+		<view class="view-box animated "  hover-class="view-box-hover bounceOutLeft">
+			第一个viewfd
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+.view-box{
+	width: 100px;
+	height: 100px;
+	background-color: #007AFF;
+	color: #FFFFFF;
+	margin: 100px;
+	font-size: 14px;
+}
+.view-box-hover{
+	background-color: green;
+}
+</style>
---
+++ qiushibaike/pages/index/index.vue
@@ -0,0 +1,31 @@
+<template>
+	<view>
+		//首页的文章开始
+		<view class="index-list">
+			<view class="index-list1">
+				<view>
+					<image src="../../satic/demo.userpic/12.jsp"></image>
+				</view>
+			</view>
+		</view>
+		//首页的文章结束
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	
+</style>
